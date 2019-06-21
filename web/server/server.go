a6cc4edffc5591c50e4d52d2fe896bf530790a84
Destin Moulton <destin.moulton@gmail.com>
2019-06-21 12:55:25 -0600

    Added static assets.

--- web/server/server.go
+++ web/server/server.go
@@ -2,9 +2,12 @@ package server
 
 import "github.com/gin-gonic/gin"
 
+// Run starts the gin server
 func Run() {
 
 	r := gin.Default()
+
+	r.Static("/assets", "../assets")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
