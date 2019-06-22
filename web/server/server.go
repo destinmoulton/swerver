0a336de2fea091c0659073220bae0aa81de48351
Destin Moulton <destin.moulton@gmail.com>
2019-06-22 09:25:42 -0600

    Added settings for the AssetsPath and the Port.

--- web/server/server.go
+++ web/server/server.go
@@ -1,17 +1,18 @@
 package server
 
 import "github.com/gin-gonic/gin"
+import "../../lib/config"
 
 // Run starts the gin server
-func Run() {
+func Run(settings config.Configuration) {
 
 	r := gin.Default()
 
-	r.Static("/assets", "./web/assets")
+	r.Static("/assets", settings.AssetsPath)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	r.Run(settings.Port)
 }
