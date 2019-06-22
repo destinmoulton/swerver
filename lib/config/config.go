42b8ef83aea83ccbf6ea71c952e0ac74510674ac
Destin Moulton <destin.moulton@gmail.com>
2019-06-22 09:20:13 -0600

    Created config file to parse the config.json

---
+++ lib/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+// Configuration struct contains the json decoded options
+type Configuration struct {
+	Port       string
+	AssetsPath string
+}
+
+// LoadConfig loads the Config struct via configPath
+func LoadConfig(configPath string) Configuration {
+	file, err := os.Open(configPath)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	decoder := json.NewDecoder(file)
+	config := Configuration{}
+	derr := decoder.Decode(&config)
+	if derr != nil {
+		log.Fatal(derr)
+	}
+	return config
+}
