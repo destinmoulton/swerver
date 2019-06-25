f6aa5a4e2dcc4862a903b814b5c81f755f7cc2d9
Destin Moulton <destin.moulton@gmail.com>
2019-06-25 17:08:26 -0600

    Added ajax methods for running scripts.

--- web/assets/js/swerver.js
+++ web/assets/js/swerver.js
@@ -41,12 +41,43 @@
             })
             .then(html => {
                 $(ajax.container).html(html);
+                setupListenersOnAjaxButtons();
             })
             .catch(err => {
                 console.error(`ajaxCall() :: Error retrieving ${url}. ${err}`);
             });
     }
 
+    function ajaxRunScript(script) {
+        const encoded = encodeURIComponent(script);
+        const url = `/ajax/run-script?script=${encoded}`;
+        fetch(url)
+            .then(resp => {
+                return resp.text();
+            })
+            .then(html => {
+                $("#sw-tty-container").prepend(html);
+            })
+            .catch(err => {
+                console.log(`ajaxRunScript() :: Error running script ${url}`);
+            });
+    }
+
+    function setupListenersOnAjaxButtons() {
+        $(".sw-ajax-button").off();
+        $(".sw-ajax-button").on("click", evt => {
+            const $target = $(evt.currentTarget);
+            const action = $target.data("action");
+            switch (action) {
+                case "run-script":
+                    const script = $target.data("script");
+                    ajaxRunScript(script);
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
     function showLoading(container) {
         $(container).html(
             "<div class='sw-loading'><img src='/assets/graphics/loading-64x64.gif'/><br>Loading...</div>"
