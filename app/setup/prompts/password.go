a1d0497f65288c3c32793388243a39ed52ecf7c3
Destin Moulton <destin.moulton@gmail.com>
2019-12-03 09:09:48 -0700

    Created password.go for pw prompts

---
+++ app/setup/prompts/password.go
@@ -0,0 +1,59 @@
+package prompts
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+)
+
+var password = ""
+
+// Password generates a prompt for the password
+func Password() string {
+
+	prompt := promptui.Prompt{
+		Label:    "Password",
+		Validate: validatePassword,
+		Mask:     '*',
+	}
+
+	password, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return ""
+	}
+	return password
+}
+
+// ConfirmPassword prompts to re type the password
+func ConfirmPassword() string {
+	prompt := promptui.Prompt{
+		Label:    "Repeat Password",
+		Validate: validateRepeatPassword,
+		Mask:     '*',
+	}
+
+	_, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return ""
+	}
+	return password
+}
+
+func validatePassword(input string) error {
+	if len(input) == 0 {
+		return errors.New("You must include a password")
+	}
+	return nil
+}
+
+func validateRepeatPassword(input string) error {
+	if password != input {
+		return errors.New("Password doesn't match")
+	}
+	return nil
+}
