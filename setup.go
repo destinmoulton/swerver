7e0c4259743bc8b10478fb441ce76609872ed3a5
Destin Moulton <destin.moulton@gmail.com>
2019-12-03 19:59:18 -0700

    Rewrote to use a map

--- setup.go
+++ setup.go
@@ -2,34 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
+	"./app/lib/config"
 	"./app/setup/prompts"
 )
 
 func main() {
 
-	port := prompts.Port()
-	password := prompts.Password()
+	// fmt.Printf("The setup has been saved to .env")
+
+	options := map[string]string{}
+	confDefaults := config.LoadConfig()
+	fmt.Println(confDefaults.Port)
+
+	options["port"] := prompts.Port(confDefaults.Port)
+	options["password"] := prompts.Password()
 	prompts.ConfirmPassword()
 
-	fmt.Printf("The setup has been saved to .env")
-
-}
-
-func writeConfig(port string, path string, services string, iplookup string) {
-
-	f, err := os.OpenFile(".env", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	defer f.Close()
-	f.WriteString(fmt.Sprintf("SWERVER_PORT=%v\n", port))
-	f.WriteString(fmt.Sprintf("SWERVER_PATH=%v\n", path))
-	f.WriteString(fmt.Sprintf("SWERVER_SERVICES=%v\n", services))
-	f.WriteString(fmt.Sprintf("SWERVER_IPLOOKUP_URL=%v\n", iplookup))
-
+	config.Save(options)
 }
