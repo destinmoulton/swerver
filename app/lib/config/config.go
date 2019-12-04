16917f276da24cdcad024698d6bc640afe118ca7
Destin Moulton <destin.moulton@gmail.com>
2019-12-03 19:46:51 -0700

    Added init method.

--- app/lib/config/config.go
+++ app/lib/config/config.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/user"
 	"path"
 	"path/filepath"
 	"strings"
 
 	"github.com/spf13/viper"
 )
+
+var configPath = ""
+var configFileName = "swerver.config"
+var configFileType = "toml"
 
 // Configuration struct contains the json decoded options
 type Configuration struct {
@@ -19,29 +24,45 @@ type Configuration struct {
 	ScriptsPath   string
 	TemplatesPath string
 	IPLookupURL   string
+	Username      string
+	PasswordHash  string
 }
 
 func init() {
-	viper.SetConfigType("toml")
-	viper.SetConfigName("swerver.config")
-	viper.AddConfigPath("$HOME/.config/swerver")
+
+	usr, uerr := user.Current()
+	if uerr != nil {
+		panic(uerr)
+	}
+
+	configPath = filepath.Join(usr.HomeDir, ".config/swerver")
+
+	if !doesConfigDirExist() {
+		fmt.Println("config dir doesn't exist")
+		createConfigDirAndFile()
+	}
+
+	viper.SetConfigType(configFileType)
+	viper.SetConfigName(configFileName)
+	viper.AddConfigPath(configPath)
 	err := viper.ReadInConfig()
 
 	if err != nil {
 		panic(fmt.Errorf("Fatal error cannot find config file: %s", err))
 	}
+
+	loadDefaults()
+
 }
 
 // LoadConfig loads the Config struct via configPath
 func LoadConfig() Configuration {
 
-	loadDefaults()
-
-	port := viper.Get("port").(string)
-	scriptsPath := viper.Get("scripts_path").(string)
-	servicesToMonitor := viper.Get("services_to_monitor").(string)
-	iplookupURL := viper.Get("ip_lookup_url").(string)
-	webPath := viper.Get("web_path").(string)
+	port := viper.GetString("port")
+	scriptsPath := viper.GetString("scripts_path")
+	servicesToMonitor := viper.GetString("services_to_monitor")
+	iplookupURL := viper.GetString("ip_lookup_url")
+	webPath := viper.GetString("web_path")
 
 	services := strings.Split(servicesToMonitor, ",")
 
@@ -65,4 +86,36 @@ func loadDefaults() {
 	viper.SetDefault("path", path)
 	viper.SetDefault("scripts_path", filepath.Join(path, "scripts"))
 	viper.SetDefault("web_path", filepath.Join(path, "web"))
+	viper.SetDefault("services_to_monitor", "")
+	viper.SetDefault("ip_lookup_url", "")
+	viper.SetDefault("username", "")
+	viper.SetDefault("password", "")
+}
+
+func doesConfigDirExist() bool {
+	info, err := os.Stat(configPath)
+	if os.IsNotExist(err) {
+
+		fmt.Println("directory doesn't exist")
+		return false
+	}
+	return info.IsDir()
+}
+
+func createConfigDirAndFile() error {
+	err := os.MkdirAll(configPath, os.ModePerm)
+
+	if err != nil {
+
+		return err
+	}
+
+	f, ferr := os.Create(filepath.Join(configPath, configFileName+"."+configFileType))
+
+	if ferr != nil {
+		return ferr
+	}
+	f.Close()
+
+	return nil
 }
