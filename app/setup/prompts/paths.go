ab5505273921ae331494d5fe62050da8ed07002c
Destin Moulton <destin.moulton@gmail.com>
2019-12-04 11:21:06 -0700

    Created Paths file

---
+++ app/setup/prompts/paths.go
@@ -0,0 +1,40 @@
+package prompts
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
+
+// ScriptsPath generates a prompt for the default port
+func ScriptsPath(initialScriptPath string) string {
+
+	prompt := promptui.Prompt{
+		Default:  initialScriptPath,
+		Label:    "ScriptsPath",
+		Validate: validatePath,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return ""
+	}
+
+	return result
+}
+
+func validatePath(input string) error {
+	if len(input) == 0 {
+		return errors.New("You must include a valid path")
+	}
+
+	_, err := os.Stat(input)
+	if os.IsNotExist(err) {
+		return errors.New("Directory doesn't exist")
+	}
+	return nil
+}
