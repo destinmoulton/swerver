c7892f72ab1453c6cdafb14241290b699f20e829
Destin Moulton <destin.moulton@gmail.com>
2019-12-04 17:31:08 -0700

    Created a random string generator

---
+++ app/lib/rando/rando.go
@@ -0,0 +1,22 @@
+package rando
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// GenerateRandomString creates a random string
+// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+func GenerateRandomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
